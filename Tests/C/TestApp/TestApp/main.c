80112ee66bc14a603bb89dc5fb3b54f4d80d9218
eobal <erich.obal@gmx.at>
2016-09-06 20:30:02 +0200

    1st commit

---
+++ Tests/C/TestApp/TestApp/main.c
@@ -0,0 +1,23 @@
+﻿
+#include <stdio.h>
+#include <time.h>
+
+int main (int argc, char *argv[])
+{
+
+	time_t rawtime;
+  	struct tm * timeinfo;
+  	printf ("Hello world!\n");
+
+
+  	time(&rawtime);
+  	timeinfo = localtime(&rawtime);
+  	printf("Current local time and date: %s", asctime(timeinfo));
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("Loop %d\n", i);
+	}
+	
+	return 0;
+}
